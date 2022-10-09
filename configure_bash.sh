1b7339be78b62135ad4438dc26517f6f1d7e5f2f
Brandon Thomas <echelon@gmail.com>
2022-10-09 00:30:22 -0400

    Update bash configure script

--- configure_bash.sh
+++ configure_bash.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 # TODO(2015-12-20): My bash dotfiles are in a flaky/disorganized state
+
+set -euxo pipefail
+
 git clone git@github.com:dotfile/bash.git ~/.config/bash
+
+ln -s ~/.config/bash/tmux.conf ~/.tmux.conf
 ln -s ~/.config/bash/inputrc ~/.inputrc
 ln -s ~/.config/bash/logout ~/.bash_logout
-ln -s ~/.config/bash/tmux.conf ~/.tmux.conf
+
 echo "Source .config/bash/bash/main from ~/.bashrc"
 
