5bbb5ae3645175f2620aa01a3d4cd09b597f9774
Brandon Thomas <echelon@gmail.com>
2026-02-22 09:27:50 +0700

    git remote setup

--- configure_git.sh
+++ configure_git.sh
@@ -5,4 +5,5 @@ set -euxo pipefail
 git config --global user.email "echelon@gmail.com"
 git config --global user.name "Brandon Thomas"
 git config --global core.editor "vim"
+git config --global push.autoSetupRemote "true"
 
